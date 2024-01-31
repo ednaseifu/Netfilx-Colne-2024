2b4d6accce2cd7b455355c377e63e7bf8158d722
e.seifu@Ednas-Air <ednaseifu16@gmail.com>
2024-01-30 23:07:22 -0600

    header and footer

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Copmonenet/Header/Header'
+import Footer from '../../Copmonenet/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
